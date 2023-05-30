e4d691cc0f2a434f85355eb0a0803dbe8a71143e
ufenekenn <ufenekenn@gmail.com>
2023-05-30 17:10:56 +0000

    Done

---
+++ api/v1/views/index.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines the index view of REST API"""
+from flask import jsonify
+
+from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    """Get API status."""
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_obj_stats():
+    """Gets the number of objects for each type."""
+    obj_stats = {
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User)
+    }
+    return jsonify(obj_stats)
